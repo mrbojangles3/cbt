14dc7eff5563373fcbfcc9db039f7ee3bafcbec4
Logan Blyth <mrbojangles3@gmail.com>
2017-02-07 16:04:02 -0500

    Added 85th percentile parsing

--- tools/radosbench_results.py
+++ tools/radosbench_results.py
@@ -15,7 +15,23 @@ def find(pattern, path):
                 results.append(os.path.join(root, name))
     return results
 
+def simple_percentile(arr, num):
+    '''
+    num should be a string value from 0.0 to 1.0
+    '''
+    arr.sort()
+    key  = int(len(arr)  * float(num)) # int() rounds down
+    return arr[key]
+    
+	
+
 def parse_rados_bench(group):
+    '''
+    Parses a group  of rados bench output files, essentially the cli output that
+    has been piped to a file
+    the files are grouped by the IO type.
+    the IO type is they key for a hash map
+    '''
     parsed_io = defaultdict(list)
     print(group[0])
     for results_file in group:
@@ -25,10 +41,13 @@ def parse_rados_bench(group):
         parsed_io[iotype]
         curr_MB = []
         curr_IOPS = []
+        curr_LAT = []
         avg_lat = -1.0
         min_lat = -1.0
         max_lat = -1.0
+        # Avoid Empty files, should make some noise here
         if os.stat(results_file).st_size <= 30:
+            print(results_file + " is EMPTY!!!!")
             continue
         with open(results_file) as f:
             # advance the fp to the first data portion
@@ -51,12 +70,13 @@ def parse_rados_bench(group):
                 try:
                     # other needed columns should be added here
                     curr_MB.append(float(line[7]))
+                    curr_LAT.append(float(line[8]))
 		    curr_IOPS.append(int(line[4]) - int(line[5]))
                 except ValueError:
                     # we hit a - in the file
                     # which means this runs needs another look
                     curr_MB.append(0.0)
-                    cur_IOPS.append(0)
+                    curr_IOPS.append(0)
                     continue
 
             #parse the latency calculated by rados bench
@@ -71,7 +91,8 @@ def parse_rados_bench(group):
                 if 'min latency' in line[0].lower():
                     min_lat=float(line[1])
 
-        parsed_io[iotype].append([sum(curr_MB)/len(curr_MB), min_lat, max_lat, avg_lat,sum(curr_IOPS)/len(curr_IOPS)]) 
+	percentile = simple_percentile(curr_LAT,'0.85') # 85th percentile of latency, a chosen upperbound
+        parsed_io[iotype].append([sum(curr_MB)/len(curr_MB), min_lat, max_lat, avg_lat,sum(curr_IOPS)/len(curr_IOPS),percentile]) 
     return parsed_io
 
 def usage():
@@ -83,8 +104,9 @@ def usage():
         3. number of clients generating IO
         4. number of data nodes
     Example invocation:
-    ./radosbench_results.py "output*" /home/username/perf-results/iomix_3_seq_read/ 4 6
+    ./radosbench_results.py "output.0.*" /home/username/perf-results/iomix_3_seq_read/ 4 6
     You need quote or escape your regex for the filename pattern.
+    Be mindful of the hostname that is actually acting as a client, it doesn't ALWAYS have client in it
     I am using python fnmatch under the covers, those regex rules apply
     '''
 
@@ -96,6 +118,7 @@ def sum_results(results,data_nodes):
         max_lat = 0.0
         avg_lat = 0.0
 	iops = 0
+	percentile = 0.0
         if key == 'delete':
             print('delete IO stats are not captured')
             continue
@@ -105,13 +128,18 @@ def sum_results(results,data_nodes):
             max_lat += data[2]
             avg_lat += data[3]
             iops += data[4]
+            percentile += data[5]
         print(key)
-	# convert rados bench seconds to milliseconds
+        # convert rados bench seconds to millisecondsrint(key)
+	try:
         print(calc_per_node_bw(bw,data_nodes),\
 		'{0:.3f}ms'.format(min_lat/len(results[key])*1000),\
                 '{0:.3f}ms'.format(max_lat/len(results[key])*1000),\
 		'{0:.3f}ms'.format(avg_lat/len(results[key])*1000),\
-		(iops))
+			(iops),\
+			'{0:.3f}ms'.format(percentile/len(results[key])*1000))
+	except ZeroDivisionError:
+		print("Hit a divide by zero, there are issues")
 
 def calc_per_node_bw(bw, nodes):
     bits_per_MiB = 8.0 * 1024 * 1024
