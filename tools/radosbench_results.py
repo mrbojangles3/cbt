a7b366421fc87ae0721cfbbfb8d55e52bf56b8ae
Logan Blyth <mrbojangles3@gmail.com>
2017-03-31 17:27:24 -0400

    Add parsing for a client with multiple output files

--- tools/radosbench_results.py
+++ tools/radosbench_results.py
@@ -2,37 +2,46 @@
 
 import os
 import fnmatch
+import glob
 import sys
 import itertools
 from collections import defaultdict
+from collections import namedtuple
+
+TestResult = namedtuple('TestResult','throughput_per_data_node, min_lat_msec, max_lat_msec, avg_lat_msec, iops, avg_percentile')
+TestSettings = namedtuple('TestSettings','read_ahead, object_size, queue_depth, pool_type, io_type')
 
 # Thanks stackoverflow!
 def find(pattern, path):
-    results = []
+    dirs_to_parse = []
     for root, dirs, files in os.walk(path):
         for name in files:
-            if fnmatch.fnmatch(name, pattern):
-                results.append(os.path.join(root, name))
-    return results
+            if fnmatch.fnmatch(name, pattern) and root not in dirs_to_parse:
+                #dirs_to_parse.append(os.path.join(root, name))
+                dirs_to_parse.append(root)
+    return dirs_to_parse
 
 def simple_percentile(arr, num):
     '''
     num should be a string value from 0.0 to 1.0
     '''
     arr.sort()
-    key  = int(len(arr)  * float(num)) # int() rounds down
+    key  = int(len(arr) * float(num)) # int() rounds down
     return arr[key]
 
-def parse_rados_bench(group):
+def parse_rados_bench(results_dir, pattern):
     '''
-    Parses a group  of rados bench output files, essentially the cli output that
+    Parses a dir  of rados bench output files, essentially the cli output that
     has been piped to a file
     the files are grouped by the IO type.
     the IO type is they key for a hash map
     '''
     parsed_io = defaultdict(list)
-    print(group[0])
-    for results_file in group:
+    print results_dir
+    for i in  glob.iglob(os.path.join(results_dir,pattern)):
+        print i
+
+    for results_file in glob.iglob(os.path.join(results_dir,pattern)):
         # get io type by using basename of file
         base_path = os.path.dirname(results_file).split('.')[0]
         iotype = base_path.split(os.sep)[-1]
@@ -89,8 +98,8 @@ def parse_rados_bench(group):
                 if 'min latency' in line[0].lower():
                     min_lat=float(line[1])
 
-	percentile = simple_percentile(curr_LAT,'0.85') # 85th percentile of latency, a chosen upperbound
-        parsed_io[iotype].append([sum(curr_MB)/len(curr_MB), min_lat, max_lat, avg_lat,sum(curr_IOPS)/len(curr_IOPS),percentile]) 
+    percentile = simple_percentile(curr_LAT,'0.85') # 85th percentile of latency, a chosen upperbound
+    parsed_io[iotype].append([sum(curr_MB)/len(curr_MB), min_lat, max_lat, avg_lat,sum(curr_IOPS)/len(curr_IOPS),percentile]) 
     return parsed_io
 
 def usage():
@@ -102,14 +111,17 @@ def usage():
         3. number of rados bench instances generating IO
         4. number of data nodes
     Example invocation:
-    ./radosbench_results.py "output.0.*" /home/username/perf-results/iomix_3_seq_read/ 4 6
+    ./radosbench_results.py "output*" /home/username/perf-results/iomix_3_seq_read/ 4 6
     You need quote or escape your regex for the filename pattern.
     Be mindful of the hostname that is actually acting as a client, it doesn't ALWAYS have client in it
     I am using python fnmatch under the covers, those regex rules apply
     '''
 
 def sum_results(results,data_nodes):
-    ''' Taking in a dictionary with iotypes as keys, 2d array as values'''
+    ''' Taking in a dictionary with iotypes (e.g. rand or seq) as keys
+    2d array as values, 
+    eg [[bw,min_lat,max_lat,avg_lat,iops,percentile],[bw,min_lat,max_lat,avg_lat,iops,percentile]]
+    '''
     for key in results.iterkeys():
         bw = 0.0
         min_lat = 0.0
@@ -130,14 +142,15 @@ def sum_results(results,data_nodes):
         print(key)
         # convert rados bench seconds to millisecondsrint(key)
         try:
-            print(calc_per_node_bw(bw,data_nodes),\
-            '{0:.3f}ms'.format(min_lat/len(results[key])*1000),\
-            '{0:.3f}ms'.format(max_lat/len(results[key])*1000),\
-            '{0:.3f}ms'.format(avg_lat/len(results[key])*1000),\
-            (iops),\
-            '{0:.3f}ms'.format(percentile/len(results[key])*1000))
+            per_dnode_tput = calc_per_node_bw(bw,data_nodes)
+            min_lat_ms = '{0:.3f}ms'.format(min_lat/len(results[key])*1000)
+            max_lat_ms = '{0:.3f}ms'.format(max_lat/len(results[key])*1000)
+            avg_lat_ms = '{0:.3f}ms'.format(avg_lat/len(results[key])*1000)
+            percentile = '{0:.3f}ms'.format(percentile/len(results[key])*1000)
         except ZeroDivisionError:
             print("Hit a divide by zero, there are issues")
+        return TestResult(throughput_per_data_node = per_dnode_tput, min_lat_msec =  min_lat_ms, max_lat_msec = max_lat_ms, avg_lat_msec = avg_lat_ms, iops = iops, avg_percentile = percentile)
+        
 
 def calc_per_node_bw(bw, nodes):
     bits_per_MiB = 8.0 * 1024 * 1024
@@ -158,14 +171,15 @@ def grouper(paths, clients):
 def main(argv):
     clients = int(argv[2])
     data_nodes = int(argv[3])
-    results_path = find(argv[0], argv[1])
-    results_path.sort()
+    results_dirs = find(argv[0], argv[1])
+    results_dirs.sort()
     # Parses all files that match the user input pattern for a given IO type
     # e.g. output.0.client-1 and output.1.client-1 are parsed if they are
     # in the same directory and match the given input pattern
-    for i in grouper(results_path,clients):
-        parsed_results = parse_rados_bench(i)
-        sum_results(parsed_results,data_nodes)
+    for i in results_dirs:
+        parsed_results = parse_rados_bench(i, argv[0])
+        results = sum_results(parsed_results,data_nodes)
+        print results
         print '-='*40
 
 
